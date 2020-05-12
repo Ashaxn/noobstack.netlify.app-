e92f4200bfd307310fb0f059d9e09fe25e0d4744
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-05-12 14:07:51 +0530

    index page polished

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,19 +3,17 @@ import "materialize-css/dist/css/materialize.min.css";
 
 class Category extends Component {
   state = {};
+
   render() {
     return (
       <div className="col s12 m4">
         <div className="card">
           <div className="card-image">
-            <img
-              src="https://raw.githubusercontent.com/Shihara-Dilshan/img/master/photo-of-imac-near-macbook-10297571.jpg"
-              alt=""
-            />
-            <span className="card-title">Test Category</span>
+            <img src={this.props.cardInfo.image} alt="" />
+            <span className="card-title">{this.props.cardInfo.title}</span>
           </div>
           <div className="card-content">
-            <p>Test Description</p>
+            <p>{this.props.cardInfo.Descripion}</p>
           </div>
           <div className="card-action">
             <a href="/" className="btn">
--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -13,12 +13,54 @@ class CategoryContainer extends Component {
             <span className="teal-text">Popular</span> Categories
           </h4>
           <div className="row">
-            <Category />
-            <Category />
-            <Category />
-            <Category />
-            <Category />
-            <Category />
+            <Category
+              cardInfo={{
+                image:
+                  "https://raw.githubusercontent.com/Shihara-Dilshan/img/master/samsung-samsung-galaxy-s6-edge-plus-edge-plus-s6-edge-47261.jpg",
+                title: "Mobile Phones",
+                Descripion: "Samsung , Apple , Huwaei and many other options",
+              }}
+            />
+            <Category
+              cardInfo={{
+                image:
+                  "https://raw.githubusercontent.com/Shihara-Dilshan/img/master/background-business-computer-connection-303383.jpg",
+                title: "Laptops",
+                Descripion: "Most powerfull laptops for your needs",
+              }}
+            />
+            <Category
+              cardInfo={{
+                image:
+                  "https://raw.githubusercontent.com/Shihara-Dilshan/img/master/photo-of-nike-shoes-1598505.jpg",
+                title: "Shoes",
+                Descripion: "Adiddas , Nike And World's famous Brands",
+              }}
+            />
+            <Category
+              cardInfo={{
+                image:
+                  "https://raw.githubusercontent.com/Shihara-Dilshan/img/master/samsung-samsung-galaxy-s6-edge-plus-edge-plus-s6-edge-47261.jpg",
+                title: "Mobile Phones",
+                Descripion: "Samsung , Apple , Huwaei",
+              }}
+            />
+            <Category
+              cardInfo={{
+                image:
+                  "https://raw.githubusercontent.com/Shihara-Dilshan/img/master/books-in-black-wooden-book-shelf-159711.jpg",
+                title: "Books",
+                Descripion: "10000+ books of most famous Authors Available",
+              }}
+            />
+            <Category
+              cardInfo={{
+                image:
+                  "https://raw.githubusercontent.com/Shihara-Dilshan/img/master/ball-court-design-game-209977.jpg",
+                title: "Sports Items",
+                Descripion: "For any type of sports.",
+              }}
+            />
           </div>
         </div>
       </section>
