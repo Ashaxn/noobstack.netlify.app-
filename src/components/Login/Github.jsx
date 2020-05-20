ec60a2bb889a33439ca70721b0897a02906e6d85
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-05-20 21:11:50 +0530

    try 1 fix

--- src/components/Login/Github.jsx
+++ src/components/Login/Github.jsx
@@ -12,8 +12,8 @@ class Github extends Component {
     picture: "",
   };
 
-  onSuccess = (response) => console.log(response);
-  onFailure = (response) => console.error(response);
+  onSuccess = (response) => console.log(response.name);
+  onFailure = (response) => console.error(response.name);
 
   render() {
     let gitContent;
